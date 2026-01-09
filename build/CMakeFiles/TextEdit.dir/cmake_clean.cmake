533c9deff843de23f23d0641e7f7503c2a6b807a
Brody Roth <brodyroth23@gmail.com>
2026-01-09 12:15:57 -0500

    Initial Commit.

---
+++ build/CMakeFiles/TextEdit.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextEdit.dir/main.cpp.o"
+  "CMakeFiles/TextEdit.dir/main.cpp.o.d"
+  "TextEdit"
+  "TextEdit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TextEdit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
