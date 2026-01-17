8edcb1c33849a747deaa876c3fcbad9bb611066d
Brody Roth <brodyroth23@gmail.com>
2026-01-16 19:22:21 -0500

    Adding open cb for open button to open the output.txt file

--- main.cpp
+++ main.cpp
@@ -26,6 +26,30 @@ void save_cb(Fl_Widget*, void* data) {
     }
 }
 
+void open_cb(Fl_Widget*, void* data){
+    Fl_Multiline_Input* input = static_cast<Fl_Multiline_Input*>(data);
+    const char* text = input->value();
+    const char* homeDir = std::getenv("HOME");
+    if(!homeDir){
+        std::cerr << "Home Environment not Set\n";
+        return;
+    }
+    std::filesystem::path filePath = std::string(homeDir) + "/output.txt";    
+    std::fstream file(filePath, std::ios::in);
+    if (file.is_open()) {
+        std::string line;
+        std::string content;
+        while (std::getline(file, line)) {
+            content += line + "\n";
+        }
+        input->value(content.c_str());
+        file.close();
+        std::cout << "File opened successfully.\n";
+    } else {
+        std::cerr << "Error opening file for reading.\n";
+    }
+}
+
 int main(int argc, char **argv) {
     Fl_Window *window = new Fl_Window(500, 500, "Text Editor");
     Fl_Menu_Bar *menubar = new Fl_Menu_Bar(0, 0, 500, 25);
@@ -33,7 +57,7 @@ int main(int argc, char **argv) {
     Fl_Menu_Item menuItems[] = {
         {"&File", 0, 0, 0, FL_SUBMENU},
             {"&New", 0, 0, 0},
-            {"&Open", 0, 0, 0},
+            {"&Open", 0, open_cb, multiline_input, 0},
             {"&Save", 0, save_cb, multiline_input, 0},
             {"&Save As...", 0, 0, 0},
             {0},
